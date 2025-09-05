892e6e22c1f36c83172557acdf0266ca2ab53eec
mohammadalkhassawneh <mohammadmalkhassawneh@gmail.com>
2025-09-05 16:00:02 +0300

    Add Status validation for DeletionRequest model

--- app/models/deletion_request.rb
+++ app/models/deletion_request.rb
@@ -5,7 +5,7 @@ class DeletionRequest < ApplicationRecord
   enum :status, { pending: 0, approved: 1, rejected: 2 }
 
   validates :reason, presence: true
-  # validates :status, presence: true
+  validates :status, presence: true
   validates :project_id, uniqueness: { scope: :user_id, conditions: -> { where(status: :pending) } }
 
   after_create :notify_admin
@@ -13,6 +13,6 @@ class DeletionRequest < ApplicationRecord
   private
 
   def notify_admin
-    Rails.logger.info "🚨 Deletion request created for project '#{project.name}' by #{user.name} (#{user.email}). Reason: #{reason}"
+    Rails.logger.info "Deletion request created for project '#{project.name}' by #{user.name} (#{user.email}). Reason: #{reason}"
   end
 end
